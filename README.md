3cd8e47bfdec06b2cb5e2bf3250855b112701a70
tmn07 <519043202@qq.com>
2016-09-07 22:58:10 +0800

    first commit (delete info

---
+++ README.md
@@ -0,0 +1,7 @@
+# HIT-spider
+|.
+|--hitwlan.py 登陆hitwlan的脚本
+|--jwts.py 过程版的。爬照片，爬成绩。
+|--main.py 重构版的。爬照片，爬成绩，选课都可。
+
+友情链接我的博客，[正在编写ing..]()
---
+++ jwts.py
@@ -0,0 +1,86 @@
+def write_down(data,filename='score.html'):
+	fp = open(filename,'w')
+	fp.write(data)
+	fp.close()
+	print('write down ok')
+
+import requests
+from bs4 import BeautifulSoup
+import os
+
+s = requests.Session()
+
+url = 'http://jwts.hit.edu.cn/loginLdap'
+
+header = {
+	'Host': 'jwts.hit.edu.cn',
+	'Connection': 'keep-alive',
+	'Content-Length': '40',
+	'Cache-Control': 'max-age=0',
+	'Origin': 'http//jwts.hit.edu.cn',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
+	'Content-Type': 'application/x-www-form-urlencoded',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Referer': 'http//jwts.hit.edu.cn/loginLdapQian',
+	'Accept-Encoding': 'gzip, deflate',
+	'Accept-Language': 'zh-CN,zh;q=0.8'
+}
+uid = input('输入你的学号')
+pwd = input('输入你的密码')
+
+post_data = {
+	'usercode': uid,
+	'password': pwd
+}
+
+r = s.post(url, headers=header ,data=post_data)
+
+if r.status_code == 200:
+	print ('login ok')
+else:
+	print('login fail')
+	exit()
+
+
+# post_data ={
+# 	'pageNo' : 1,
+# 	'pageSize' : 250,
+# 	'pageCount':1
+# }
+# r = s.post('http://jwts.hit.edu.cn/cjcx/queryQmcj',data=post_data)
+
+# if r.status_code == 200:
+# 	print ('get score ok')
+# else:
+# 	print('get score fail')
+# 	exit()
+
+# write_down(r.text)
+
+
+# main_soup = BeautifulSoup(r.text, 'lxml')
+# form = main_soup.find(class_="bot_line")
+# all_data = form.find_all('tr')
+for c in range(1,3):
+
+	img_url = "http://jwts.hit.edu.cn/xswhxx/showPhoto?xh=1152310" + str(c)
+
+	ddir = './pic/gm15'+str(c)
+	os.mkdir(ddir)
+# if not os.path.exists(ddir):
+    # 1522101
+
+
+	for i in range(1,20):
+		if i < 10:
+			flag = '0'
+		else:
+			flag = ''
+		r = s.get(img_url+flag+str(i))
+		with open(ddir+'/pic'+str(i)+'.jpg','wb') as f:
+			f.write(r.content)
+		print('class '+str(c)+' num '+str(i)+'download  over')
+
+
+	
