1942ab39d764508e65d355c3a7eb755cb4865393
Tmn07 <519043202@qq.com>
2016-11-07 22:15:42 +0800

    选课，退课。

--- jwc.py
+++ jwc.py
@@ -72,9 +72,51 @@ class hit_jwts(object):
         r = self.s.get(url)
         self.write_down(r.content, 'photo.jpg', 'wb')
 
+    def xuanke(self,cid,lb,qz=""):
+        url = "http://jwts.hit.edu.cn/xsxk/saveXsxk"
+        data = {
+            'rwh' : cid,
+            'pageXklb':'xx',
+            'qz': qz,
+        }
+        r = self.s.post(url,data)
+        return r
+
+    def tuike(self,cid="",lb=""):
+        url = "http://jwts.hit.edu.cn/xsxk/saveXstk"
+        header = {
+            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding':'gzip, deflate',
+            'Accept-Language':'zh-CN,zh;q=0.8',
+            'Cache-Control':'max-age=0',
+            'Connection':'keep-alive',
+            'Content-Length':'102',
+            'Content-Type':'application/x-www-form-urlencoded',
+            'Host':'jwts.hit.edu.cn',
+            'Origin':'http://jwts.hit.edu.cn',
+            'Referer':'http://jwts.hit.edu.cn/xsxk/queryYxkc',
+            'Upgrade-Insecure-Requests':'1',
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36'
+        }
+        data = {
+            'rwh' : cid,
+            'pageXklb' : lb,
+            'pageXnxq':'2016-20172',
+            'pageNj': '',
+            'pageYxdm': '',
+            'pageZydm': '',
+            'pageKcmc': '',
+        }
+        r = self.s.post(url,headers=header, data=data)
+        return r
+
 
 if __name__ == '__main__':
-    # 第一个参数学号，第一个参数密码
+    # 第一个参数学号，第二个参数密码
     c = hit_jwts('xxx', 'xxxx')
-    c.score()
-    c.getPhoto('1140340116')
+    # c.score()
+    # c.getPhoto('1140340116')
+
+    # c.xuanke("2016-2017-2-13SE28001200-001","xx")
+    # c.xuanke("2016-2017-1-GO00300400-001","qxrx")
+    # c.tuike("2016-2017-1-GO00300400-001","qxrx")
