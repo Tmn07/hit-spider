d8e6372cb642629a37777aebf6eceb926b4f4cdb
Tmn07 <519043202@qq.com>
2016-12-24 20:01:00 +0800

    概率论专属爬虫&可视化

--- jwc.py
+++ jwc.py
@@ -3,6 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 from login import *
 import time
+import MySQLdb
+
+
 
 class hit_jwts(object):
     """ 程序主类
@@ -91,13 +94,15 @@ class hit_jwts(object):
 
     def cjxxview(self):
         filep = open("cjxx.txt","w")
-        start_id = 6666780
+        # 6666636
+        start_id = 6669600
         baseurl = "http://jwts.hit.edu.cn/cjcx/queryCjxxView?id="
         f = 0
         while f<30:
             url = baseurl+str(start_id)
             r = self.s.get(url)
             if r.headers.get('Content-Length',0) == "1842":
+                # 2472
                 print start_id,"null"
                 f+=1
                 start_id += 1
@@ -111,6 +116,57 @@ class hit_jwts(object):
             start_id += 1
             time.sleep(2)
 
+
+    def gay(self):
+        db = MySQLdb.connect("127.0.0.1","root","root","jwts")
+
+        # 使用cursor()方法获取操作游标 
+        cursor = db.cursor()
+
+        # 6666620  6666794
+        # 6666294  6670647
+        start_id = 6670634
+        # baseurl = "http://jwts.hit.edu.cn/cjcx/queryCjxxView?rwh=2016-2017-1-13GN12000300-017&id="
+
+        baseurl = "https://vpn.hit.edu.cn/cjcx/,DanaInfo=jwts.hit.edu.cn+queryCjxxView?rwh=2016-2017-1-13GN12000300-017&id=667063"
+        f = 0
+        # i = 0
+        while f<30:
+            url = baseurl+str(start_id)
+            r = self.s.get(url)
+            # 13GN12000300
+            if r.headers.get('Content-Length',0) == "2472":
+                # 2472
+                print start_id,"null"
+                f+=1
+                start_id += 1
+                time.sleep(0.5)
+                # filep.write(str(start_id)+"\tnull\n")
+                continue
+            soup = BeautifulSoup(r.content,"lxml")
+            data = soup.find_all('td')
+            # print data[2]
+            if data[2].string=="13GN12000300":
+                try:
+                    sql = "INSERT INTO `jwts`.`gay` (`id`, `rwh`, `score`, `item1`, `item2`, `item3`) VALUES('%d', '%s', '%f', '%f', '%f', '%f');" % (start_id , data[0].string[-3:], float(data[7].string),float(data[9].string),float(data[11].string),float(data[13].string))
+                    # print float(data[7].string)
+                except Exception, e:
+                    # raise e
+                    if data[7].string == u"缓考":
+                        status = 1
+                    else:
+                        status = 2
+                    sql = "INSERT INTO `jwts`.`gay` (`id`, `rwh`, `status`) VALUES('%d', '%s', '%d');" % (start_id , data[0].string[-3:], status)
+                print sql
+                cursor.execute(sql)
+
+            print start_id,data[0],data[3],data[7]
+            # filep.write(str(start_id)+str(data[0])+str(data[3])+str(data[7])+"\n")
+            start_id += 1
+            # i+=1
+            time.sleep(0.5)
+
+        db.close()
 
     def tuike(self,cid="",lb=""):
         url = "http://jwts.hit.edu.cn/xsxk/saveXstk"
@@ -143,8 +199,10 @@ class hit_jwts(object):
 
 if __name__ == '__main__':
     # 第一个参数学号，第二个参数密码
-    c = hit_jwts('xxxx', 'xxxx')
-    c.cjxxview()
+    c = hit_jwts('xxx', 'xxx')
+    # c.cjxxview()
+    c.gay()
+
     # c.score()
     # c.getPhoto('1140340116')
 
