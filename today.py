5d950847b56f6f8d46d773a9df0313c2504abb52
rainharp <rainharp1990@gmail.com>
2017-06-15 05:58:15 +0800

    today.py update

--- today.py
+++ today.py
@@ -3,36 +3,62 @@
 Created on Fri Dec 25 19:45:39 2015
 @author: Tmn07
 """
-import urllib2  
+
 import random
-import time 
-# 传入'推荐本文'对应的url
-target = '/commend/126309_1.htm'
+import time
+import requests
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
 
-base = 'http://today.hit.edu.cn'
-url = base + target
-user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'  
-headers = { 'User-Agent' : user_agent }  
-def shua(k,headers):
-    for i in range(k):
-        a = random.randint(1,255)
-        b = random.randint(0,255)
-        c = random.randint(0,255)
-        d = random.randint(0,255)
-        ipAddress = "%d.%d.%d.%d" % (a,b,c,d)
-        print ipAddress
+def get_recommend_url(page_url):
+    '''
+    获取"推荐本文"对应的链接
+    '''
+    page = requests.get(page_url).content                   # requests.get  获取页面
+    soup = BeautifulSoup(page, 'html.parser')               # BeautifulSoup 解析页面
+    iframe_url = soup.find_all('center')[1].iframe['src']   # 获取 iframe 链接
+    recommend_url = 'http://today.hit.edu.cn' + iframe_url.replace('0.htm','1.htm')
+    return recommend_url
+
+def get_recommend_count(page_url):
+    '''
+    获取新闻推荐数
+    '''
+    page = requests.get(page_url).content
+    soup = BeautifulSoup(page, 'html.parser')
+    iframe_url = 'http://today.hit.edu.cn' + soup.find_all('center')[1].iframe['src']
+    iframe_code = requests.get(iframe_url).content
+    iframe_soup = BeautifulSoup(iframe_code, 'html.parser')
+    recommend_count = iframe_soup.find_all('div',{'class','topBox'})[0].text
+    recommend_count = recommend_count.replace('\t','').replace('\r','').replace('\n','')
+    return int(recommend_count)
     
-        headers["X-Forwarded-For"] = ipAddress
-        
-        time.sleep(0.5)
-        
-        request = urllib2.Request(url, "", headers)  
-        try:    
-            urllib2.urlopen(request)
-        except urllib2.HTTPError,e:
-            print e.code
-        
-        print i
+def ip_generator():
+    '''
+    生成随机的IP地址
+    '''
+    a = random.randint(1,255)
+    b = random.randint(0,255)
+    c = random.randint(0,255)
+    d = random.randint(0,255)
+    ipAddress = "%d.%d.%d.%d" % (a,b,c,d)
+    print ipAddress
+    return ipAddress
 
-# 第一个参数为刷票次数.
-shua(10,headers)
+def shua(page_url,k):
+    url = get_recommend_url(page_url)           # 获取"推荐本文"的链接
+    for i in range(k):
+        headers = { "User-Agent": UserAgent().random, "X-Forwarded-For": ip_generator() }
+        try:    
+            request = requests.get(url, headers = headers)
+        except requests.exceptions.RequestException, e:
+            print e.code
+            
+        time.sleep(random.random())        
+        print i+1
+
+
+page_url = "http://today.hit.edu.cn/news/2017/06-05/4762308160RL1.htm"   # 新闻页面的链接
+print get_recommend_count(page_url)     # 刷票前推荐数
+shua(page_url,3)
+print get_recommend_count(page_url)     # 刷票后推荐数
